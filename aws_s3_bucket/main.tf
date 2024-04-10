c5124077e9f796c286e7c1cdf7257b812efa8895
rav94 <fernandoravindu1994@gmail.com>
2024-04-10 10:06:58 +0530

    Remove policy s3

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -11,26 +11,3 @@ resource "aws_s3_bucket" "bucket" {
 
   tags = var.tags
 }
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-    bucket = aws_s3_bucket.bucket.id
-
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${var.bucket_name}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
